e7a537b3590faa065f79779b44d688e0787afe2e
Damien Duboeuf <d.duboeuf@glowbl.com>
2014-02-07 17:51:36 +0100

    Add GameRegistery name for MC 1.7

--- common/rebelkeithy/mods/keithyutils/metablock/MetaBlock.java
+++ common/rebelkeithy/mods/keithyutils/metablock/MetaBlock.java
@@ -32,7 +32,13 @@ public class MetaBlock extends Block {
 	
 	//public static SubBlock air = new SubBlock(0, 0, "").setHardness(0).setResistance(0);
 	
+	@Deprecated
 	public static void registerID(int id)
+	{
+		MetaBlock.registerID (id, null);
+	}
+	
+	public static void registerID(int id, String name)
 	{
 		if(registeredIDs == null)
 			registeredIDs = new ArrayList();
@@ -43,7 +49,7 @@ public class MetaBlock extends Block {
 		Block block = Block.blocksList[id];
 		if(block instanceof MetaBlock)
 		{
-			GameRegistry.registerBlock(block, ItemMetaBlock.class);
+			GameRegistry.registerBlock(block, ItemMetaBlock.class, name);
 			registeredIDs.add(id);
 		}
 	}
@@ -51,7 +57,6 @@ public class MetaBlock extends Block {
 	public MetaBlock(int id) 
 	{
 		super(id, Material.rock);
-		System.out.println("test");
 		subBlocks = new SubBlock[16];
 		//for(int i = 0; i > 16; i++)
 		//	subBlocks[i] = air;
